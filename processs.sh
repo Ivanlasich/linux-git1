485057176192604a032c364a03fd193922f364ba
Лазичный_Иван <Ivanlasich@linux1.ru-central1.internal>
2020-11-01 17:59:42 +0000

    commit!

--- processs.sh
+++ processs.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/gnuplot
 set terminal png size 300,400
 set datafile separator comma
-f(x) = m*x +b
-fit f(x) '/home/datamove/hotels.csv' using 12:18 via m,b
+f(x) = m*x + b
+fit f(x) '/home/datamove/hotels.csv' using 18:(12 > 0 ? 12 : 1/0) via m, b
 set output 'w_vs_h_fit.png'
-plot '/home/datamove/hotels.csv' using 12:18 title 'Height vs. Weight' with points, f(x) title 'fit'
+plot '/home/datamove/hotels.csv' using 18:(12 > 0 ? 12 : 1/0) title 'Height vs. Weight' with points, f(x) title 'fit'
