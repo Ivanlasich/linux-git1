1dba1ae35195772af8f0e778c7fd1a19d47406f9
Лазичный_Иван <Ivanlasich@linux1.ru-central1.internal>
2020-11-01 13:24:28 +0000

    last

--- process.sh
+++ process.sh
@@ -10,12 +10,3 @@ f(x) = m*x +b
 fit f(x) '/home/datamove/hotels.csv' using 12:18 via m,b
 set output 'w_vs_h_fit.png'
 plot '/home/datamove/hotels.csv' using 12:18 title 'Height vs. Weight' with points, f(x) title 'fit'
-
-
-
-
-
-
-
-
-
