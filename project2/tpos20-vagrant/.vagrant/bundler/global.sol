3c17d0766a2421feb5c9ee96169afe38e199ce09
Лазичный_Иван <Ivanlasich@linux1.ru-central1.internal>
2020-12-10 11:17:12 +0000

    3rd commit!

---
+++ project2/tpos20-vagrant/.vagrant/bundler/global.sol
@@ -0,0 +1,1 @@
+{"dependencies":[["vagrant-hostmanager",["= 1.8.9"]],["net-ssh",[">= 2.6.5"]],["net-scp",["~> 1.1"]],["log4r",["~> 1.1"]],["vagrant-scp",["= 0.5.7"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.28.0"]]],"checksum":"1e862a299eb7570d6909cadc7190dadbfc064338e0954ec50e3733309c3b566d","vagrant_version":"2.2.10"}
